5ca81b0b17d98b0b844e58af7896a37984074669
youssef-salih <115431342+youssef-salih@users.noreply.github.com>
2026-01-01 23:48:10 +0100

    init

---
+++ src/main/java/com/jobintech/devopsback/DevopsBackApplication.java
@@ -0,0 +1,13 @@
+package com.jobintech.devopsback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsBackApplication.class, args);
+    }
+
+}
